eede9aefd3b4c9b10775ec70830a2ec25efc3704
ColdWindScholar <3590361911@qq.com>
2026-06-29 01:45:58 +0800

    add build system

--- envsetup.sh
+++ envsetup.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 check_python3 (){
   python3=$(which python3)
+  echo "[1/3]Checking Python3."
   if [ -z "$python3" ]; then
     echo "Python3's missing, install by 'sudo apt install python3-full'"
-    exit
+    exit 1
   fi
+  echo "[2/3]Install Required Packages."
   $python3 -m pip install -r requirements.txt
   if [ "$?" != "0" ];then
     echo "Failed to install required python packages."
+    exit 1
   fi
-  echo "Check pass."
+  echo "[3/3]All Done!"
 }
 check_python3
