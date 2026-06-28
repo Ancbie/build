9003999117d636378c05950a5db5afabbcd37c59
ColdWindScholar <3590361911@qq.com>
2026-06-29 02:56:03 +0800

    add build system

--- main.py
+++ main.py
@@ -13,8 +13,6 @@ class Builder:
         self.source_dirs = ["wolfssl", 'atl', 'art', "bionic", "libopensles", "atl-gui"]
 
     def execute(self, command: list, working_dir:str|None=None, shell:bool=False) -> int:
-        if command[0] == 'pkexec':
-            shell = True
         ret = subprocess.Popen(command, cwd=working_dir, stderr=subprocess.STDOUT, shell=shell)
         assert ret.returncode == 0, f'Run {command} failed.'
         return ret.returncode
@@ -32,8 +30,7 @@ class Builder:
         if os.getuid() == 0:
             self.execute(['apt', 'install', *self.apt_packages])
         else:
-            assert shutil.which("pkexec"), "pkexec missing."
-            self.execute(["pkexec", 'apt', 'install', *self.apt_packages])
+            self.execute(["sudo", 'apt', 'install', *self.apt_packages])
 
     def check_source_code(self):
         for i in self.source_dirs:
@@ -45,7 +42,7 @@ class Builder:
         self.execute(['autoreconf', '-i'], working_dir=working_path)
         self.execute(["sh","./configure", "--enable-shared", "--disable-opensslall", "--disable-opensslextra", "--enable-aescbc-length-checks", "--enable-curve25519", "--enable-ed25519", "--enable-ed25519-stream", "--enable-oldtls","--enable-base64encode","--enable-tlsx","--enable-scrypt", "--disable-examples", "--enable-crl", "--enable-jni", "--enable-sessioncerts"],working_dir=working_path)
         self.execute(['make'], working_dir=working_path)
-        self.execute(['pkexec', 'make', 'install'], working_dir=working_path)
+        self.execute(['sudo', 'make', 'install'], working_dir=working_path)
     def build_meson_projects(self):
         for i in ['atl', "bionic", 'libopensles']:
             self.build_meson(i)
@@ -54,12 +51,12 @@ class Builder:
         self.execute(["meson", "setup", "builddir"], working_dir=working_path)
         working_path = os.path.realpath(os.path.join(working_path, 'builddir'))
         self.execute(['meson', 'compile'], working_dir=working_path)
-        self.execute(['pkexec','meson', 'install'], working_dir=working_path)
+        self.execute(['sudo','meson', 'install'], working_dir=working_path)
 
     def build_art(self):
         working_path = os.path.realpath('art')
         self.execute(['make', "____LIBDIR=lib"], working_dir=working_path)
-        self.execute(["pkexec",'make', "____LIBDIR=lib", "install"], working_dir=working_path)
+        self.execute(["sudo",'make', "____LIBDIR=lib", "install"], working_dir=working_path)
 
 if __name__ == "__main__":
     builder = Builder()
